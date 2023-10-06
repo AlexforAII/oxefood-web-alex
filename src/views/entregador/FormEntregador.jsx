9a38432dcc531aa07a15813b577e00905b48e598
aluno <aluno@ifpe>
2023-10-06 16:13:06 -0300

    Atualização de front-end

--- src/views/entregador/FormEntregador.jsx
+++ src/views/entregador/FormEntregador.jsx
@@ -1,10 +1,14 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import InputMask from 'react-input-mask';
+import { useLocation } from "react-router-dom";
 import { Button, Container, Divider, Form, Icon } from 'semantic-ui-react';
 import MenuSistema from '../../MenuSistema';
 
-export default function FormEntregador () {
+export default function FormEntregador() {
+    const { state } = useLocation();
+    const [idEntregador, setIdEntregador] = useState();
+
     const [nome, setNome] = useState();
     const [cpf, setCpf] = useState();
     const [RG, setRG] = useState();
@@ -20,35 +24,81 @@ export default function FormEntregador () {
     const [CEP, setCEP] = useState();
     const [UF, setUF] = useState();
     const [complemento, setcomplemento] = useState();
-    
+
+    useEffect(() => {
+
+        if (state != null && state.id != null) {
+
+            axios.get("http://localhost:8082/api/entregador/" + state.id)
+                .then((response) => {
+                    setIdEntregador(response.data.id)
+                    setNome(response.data.nome)
+                    setCpf(response.data.cpf)
+                    setRG(response.data.RG)
+                    setDTNascimento(formatarData(response.data.DTNascimento))
+                    setFoneCelular(response.data.foneCelular)
+                    setQTDEntregasRealizadas(response.data.QTDEntregasRealizadas)
+                    setvalorPorFrete(response.data.valorPorFrete)
+                    setrua(response.data.rua)
+                    setnumero(response.data.numero)
+                    setbairro(response.data.bairro)
+                    setcidade(response.data.cidade)
+                    setCEP(response.data.CEP)
+                    setUF(response.data.UF)
+                    complemento(response.data.setcomplemento)
+
+
+                })
+        }
+    }, [state])
+    function formatarData(dataParam) {
+
+        if (dataParam === null || dataParam === '' || dataParam === undefined) {
+            return ''
+        }
+
+        let arrayData = dataParam.split('-');
+        return arrayData[2] + '/' + arrayData[1] + '/' + arrayData[0];
+    }
+
     function salvar() {
 
-		let entregadorRequest = {
-		     nome: nome,
-		     cpf: cpf,
-             RG: RG,
-             DTNascimento: DTNascimento,
-             foneCelular: FoneCelular,
-             QTDEntregasRealizadas, QTDEntregasRealizadas,
-             foneFixo: FoneFixo,
-		     valorPorFrete: valorPorFrete,
-		     rua: rua,
-		     numero: numero,
-             bairro: bairro,
-             cidade: cidade,
-             CEP: CEP,
-             UF: UF,
-             complemento: complemento
-		}
-	
-		axios.post("http://localhost:8082/api/cliente", entregadorRequest)
-		.then((response) => {
-		     console.log('Cliente cadastrado com sucesso.')
-		})
-		.catch((error) => {
-		     console.log('Erro ao incluir o um cliente.')
-		})
-	}
+        let entregadorRequest = {
+            nome: nome,
+            cpf: cpf,
+            RG: RG,
+            DTNascimento: DTNascimento,
+            foneCelular: foneCelular,
+            QTDEntregasRealizadas, QTDEntregasRealizadas,
+            foneFixo: foneFixo,
+            valorPorFrete: valorPorFrete,
+            rua: rua,
+            numero: numero,
+            bairro: bairro,
+            cidade: cidade,
+            CEP: CEP,
+            UF: UF,
+            complemento: complemento
+        }
+
+        if (idEntregador != null) { //Alteração:
+            axios.put("http://localhost:8082/api/entregador/" + idEntregador, entregadorRequest)
+                .then((response) => {
+                    console.log('Entregador alterado com sucesso.')
+                })
+                .catch((error) => {
+                    console.log('Erro ao alter um entregador.')
+                })
+        } else { //Cadastro:
+            axios.post("http://localhost:8082/api/entregador", entregadorRequest)
+                .then((response) => {
+                    console.log('Entregador cadastrado com sucesso.')
+                })
+                .catch((error) => {
+                    console.log('Erro ao incluir o entregador.')
+                })
+        }
+    }
 
 
     return (
@@ -56,15 +106,20 @@ export default function FormEntregador () {
         <div>
             <MenuSistema />
 
-            <div style={{marginTop: '3%'}}>
+            <div style={{ marginTop: '3%' }}>
 
                 <Container textAlign='justified' >
 
-                    <h2> <span style={{color: 'darkgray'}}> Entegrador &nbsp;<Icon name='angle double right' size="small" /> </span> Cadastro </h2>
+                    {idEntregador === undefined &&
+                        <h2> <span style={{ color: 'darkgray' }}> Entregador &nbsp;<Icon name='angle double right' size="small" /> </span> Cadastro</h2>
+                    }
+                    {idEntregador != undefined &&
+                        <h2> <span style={{ color: 'darkgray' }}> Entregador &nbsp;<Icon name='angle double right' size="small" /> </span> Alteração</h2>
+                    }
 
                     <Divider />
 
-                    <div style={{marginTop: '4%'}}>
+                    <div style={{ marginTop: '4%' }}>
 
                         <Form>
 
@@ -84,71 +139,71 @@ export default function FormEntregador () {
                                     fluid
                                     label='CPF'>
                                     <InputMask
-                                    required
-                                    mask="999.999.999-99"
-                                    value={cpf}
-                                    onChange={e => setCpf(e.target.value)}
-                                    /> 
+                                        required
+                                        mask="999.999.999-99"
+                                        value={cpf}
+                                        onChange={e => setCpf(e.target.value)}
+                                    />
                                 </Form.Input>
 
                                 <Form.Input
-                                   fluid
-                                   label='RG'
-                                   value={RG}
-                                   onChange={e => setRG(e.target.value)}
+                                    fluid
+                                    label='RG'
+                                    value={RG}
+                                    onChange={e => setRG(e.target.value)}
                                 >
-                               
+
                                 </Form.Input>
 
                             </Form.Group>
-                            
+
                             <Form.Group>
 
                                 <Form.Input
                                     fluid
                                     label='DT Nascimento'
                                     width={6}>
-                                    <InputMask 
-                                    mask="99/99/9999" 
-                                    maskChar={null}
-                                    placeholder="Ex: 20/03/1985"
-                                    value={DTNascimento}
-                                    onChange={e => setDTNascimento(e.target.value)}
-                                    /> 
+                                    <InputMask
+                                        mask="99/99/9999"
+                                        maskChar={null}
+                                        placeholder="Ex: 20/03/1985"
+                                        value={DTNascimento}
+                                        onChange={e => setDTNascimento(e.target.value)}
+                                    />
                                 </Form.Input>
 
                                 <Form.Input
-                                required
-                                fluid
-                                label='Fone Celular'
-                                width={6}>
-                                    <InputMask 
+                                    required
+                                    fluid
+                                    label='Fone Celular'
+                                    width={6}>
+                                    <InputMask
                                         mask="(99) 9999.9999"
                                         value={foneCelular}
                                         onChange={e => setFoneCelular(e.target.value)}
-                                    /> 
+                                    />
                                 </Form.Input>
 
                                 <Form.Input
                                     fluid
                                     label='Fone Fixo'
                                     width={6}>
-                                    <InputMask 
+                                    <InputMask
                                         mask="(99) 9999.9999"
                                         value={foneFixo}
                                         onChange={e => setFoneFixo(e.target.value)}
-                                    /> 
+                                    />
                                 </Form.Input>
 
                                 <Form.Input
                                     fluid
                                     label='QTD Entregas Realizadas'
                                 >
-                                    <InputMask 
+                                    <InputMask
                                         mask="99/99/9999"
                                         value={QTDEntregasRealizadas}
                                         onChange={e => setQTDEntregasRealizadas(e.target.value)}
-                                    /> 
+                                    />
                                 </Form.Input>
 
                                 <Form.Input
@@ -156,92 +211,92 @@ export default function FormEntregador () {
                                     label='Valor Por Frete'
                                     value={valorPorFrete}
                                     onChange={e => setvalorPorFrete(e.target.value)}
-                                > 
+                                >
                                 </Form.Input>
 
                             </Form.Group>
 
                             <Form.Group>
 
-                            <Form.Input
+                                <Form.Input
                                     fluid
                                     label='Rua'
                                     value={rua}
                                     onChange={e => setrua(e.target.value)}
-                                > 
-                                 </Form.Input>
+                                >
+                                </Form.Input>
 
-                                 <Form.Input
+                                <Form.Input
                                     fluid
                                     label='Número'
                                     value={numero}
                                     onChange={e => setnumero(e.target.value)}
-                                > 
-                                 </Form.Input>
+                                >
+                                </Form.Input>
 
                             </Form.Group>
 
                             <Form.Group>
 
-                            <Form.Input
+                                <Form.Input
                                     fluid
                                     label='Bairro'
                                     value={bairro}
                                     onChange={e => setbairro(e.target.value)}
-                                > 
-                                 </Form.Input>
+                                >
+                                </Form.Input>
 
-                                 <Form.Input
+                                <Form.Input
                                     fluid
                                     label='Cidade'
                                     value={cidade}
                                     onChange={e => setcidade(e.target.value)}
-                                > 
-                                 </Form.Input>
+                                >
+                                </Form.Input>
 
-                                 <Form.Input
+                                <Form.Input
                                     fluid
                                     label='CEP'
                                     value={CEP}
                                     onChange={e => setCEP(e.target.value)}
-                                > 
-                                 </Form.Input>
+                                >
+                                </Form.Input>
 
                             </Form.Group>
 
                             <Form.Group>
 
-                            <Form.Input
+                                <Form.Input
                                     fluid
                                     label='UF'
                                     value={UF}
                                     onChange={e => setUF(e.target.value)}
-                                > 
-                                 </Form.Input>
+                                >
+                                </Form.Input>
 
                             </Form.Group>
 
                             <Form.Group>
 
-                            <Form.Input
+                                <Form.Input
                                     fluid
                                     label='Complemento'
                                     value={complemento}
                                     onChange={e => setcomplemento(e.target.value)}
-                                > 
-                                 </Form.Input>
+                                >
+                                </Form.Input>
 
                             </Form.Group>
 
                             <Form.Group>
-                <Form.Field label="Ativo:"></Form.Field>
-                <Form.Radio id="sim" name="ativo" value={"Sim"} label="Sim" />
-                <Form.Radio id="nao" name="ativo" value={"Não"} label="Não" />
-              </Form.Group>
+                                <Form.Field label="Ativo:"></Form.Field>
+                                <Form.Radio id="sim" name="ativo" value={"Sim"} label="Sim" />
+                                <Form.Radio id="nao" name="ativo" value={"Não"} label="Não" />
+                            </Form.Group>
 
                         </Form>
-                        
-                        <div style={{marginTop: '4%'}}>
+
+                        <div style={{ marginTop: '4%' }}>
 
                             <Button
                                 type="button"
@@ -254,25 +309,25 @@ export default function FormEntregador () {
                                 <Icon name='reply' />
                                 Voltar
                             </Button>
-                                
+
                             <Button
-				inverted
-				circular
-				icon
-				labelPosition='left'
-				color='blue'
-				floated='right'
-				onClick={() => salvar()}
-			>
-				<Icon name='save' />
-				Salvar
-			</Button>
+                                inverted
+                                circular
+                                icon
+                                labelPosition='left'
+                                color='blue'
+                                floated='right'
+                                onClick={() => salvar()}
+                            >
+                                <Icon name='save' />
+                                Salvar
+                            </Button>
 
 
                         </div>
 
                     </div>
-                    
+
                 </Container>
             </div>
         </div>
--- src/views/produto/FormProduto.jsx
+++ src/views/produto/FormProduto.jsx
@@ -1,36 +1,67 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import InputMask from 'react-input-mask';
+import { useLocation } from "react-router-dom";
 import { Button, Container, Divider, Form, Icon } from 'semantic-ui-react';
 import MenuSistema from '../../MenuSistema';
 
-export default function FormProduto () {
+export default function FormProduto() {
+    const { state } = useLocation();
+    const [idProduto, setIdProduto] = useState();
+
     const [titulo, settitulo] = useState();
-   const [codigoDoProduto, setcodigoDoProduto] = useState();
-   const [descricao, setdescricao] = useState();
-   const [valorUnitario, setvalorUnitario] = useState();
-   const [TempoDeEntregaMinimaEmMinutos, setTempoDeEntregaMinimaEmMinutos] = useState();
-   const [TempoDeEntregaMaximaEmMinutos, setTempoDeEntregaMaximaEmMinutos] = useState();
+    const [codigoDoProduto, setcodigoDoProduto] = useState();
+    const [descricao, setdescricao] = useState();
+    const [valorUnitario, setvalorUnitario] = useState();
+    const [TempoDeEntregaMinimaEmMinutos, setTempoDeEntregaMinimaEmMinutos] = useState();
+    const [TempoDeEntregaMaximaEmMinutos, setTempoDeEntregaMaximaEmMinutos] = useState();
 
-   function salvar() {
+    useEffect(() => {
 
-    let produtoRequest = {
-        titulo:titulo,
-        codigoDoProduto: codigoDoProduto,
-        descricao:descricao,
-        valorUnitario: valorUnitario,
-        TempoDeEntregaMinimaEmMinutos: TempoDeEntregaMinimaEmMinutos,
-        TempoDeEntregaMaximaEmMinutos: TempoDeEntregaMaximaEmMinutos
+        if (state != null && state.id != null) {
+
+            axios.get("http://localhost:8082/api/produto/" + state.id)
+                .then((response) => {
+                    setIdProduto(response.data.id)
+                    settitulo(response.data.titulo)
+                    setcodigoDoProduto(response.data.codigoDoProduto)
+                    setdescricao(response.data.descricao)
+                    setvalorUnitario(response.data.valorUnitario)
+                    setTempoDeEntregaMinimaEmMinutos(response.data.TempoDeEntregaMinimaEmMinutos)
+                    setTempoDeEntregaMaximaEmMinutos(response.data.TempoDeEntregaMaximaEmMinutos)
+                })
+        }
+    }, [state])
+
+    function salvar() {
+
+        let produtoRequest = {
+            titulo: titulo,
+            codigoDoProduto: codigoDoProduto,
+            descricao: descricao,
+            valorUnitario: valorUnitario,
+            TempoDeEntregaMinimaEmMinutos: TempoDeEntregaMinimaEmMinutos,
+            TempoDeEntregaMaximaEmMinutos: TempoDeEntregaMaximaEmMinutos
+        }
+
+        if (idProduto != null) { //Alteração:
+            axios.put("http://localhost:8082/api/produto/" + idProduto, produtoRequest)
+                .then((response) => {
+                    console.log('Produto alterado com sucesso.')
+                })
+                .catch((error) => {
+                    console.log('Erro ao alter um Produto.')
+                })
+        } else { //Cadastro:
+            axios.post("http://localhost:8082/api/produto", produtoRequest)
+                .then((response) => {
+                    console.log('Produto cadastrado com sucesso.')
+                })
+                .catch((error) => {
+                    console.log('Erro ao incluir o produto.')
+                })
+        }
     }
-
-    axios.post("http://localhost:8082/api/cliente", produtoRequest)
-    .then((response) => {
-         console.log('Cliente cadastrado com sucesso.')
-    })
-    .catch((error) => {
-         console.log('Erro ao incluir o um cliente.')
-    })
-}
 
 
 
@@ -39,15 +70,20 @@ export default function FormProduto () {
         <div>
             <MenuSistema />
 
-            <div style={{marginTop: '3%'}}>
+            <div style={{ marginTop: '3%' }}>
 
                 <Container textAlign='justified' >
 
-                    <h2> <span style={{color: 'darkgray'}}> Produto &nbsp;<Icon name='angle double right' size="small" /> </span> Cadastro </h2>
+                    {idProduto === undefined &&
+                        <h2> <span style={{ color: 'darkgray' }}> Produto &nbsp;<Icon name='angle double right' size="small" /> </span> Cadastro</h2>
+                    }
+                    {idProduto != undefined &&
+                        <h2> <span style={{ color: 'darkgray' }}> Produto &nbsp;<Icon name='angle double right' size="small" /> </span> Alteração</h2>
+                    }
 
                     <Divider />
 
-                    <div style={{marginTop: '4%'}}>
+                    <div style={{ marginTop: '4%' }}>
 
                         <Form>
 
@@ -58,19 +94,19 @@ export default function FormProduto () {
                                     fluid
                                     label='Título'
                                 >
-                                 <InputMask 
+                                    <InputMask
                                         placeholder="Informe o título do produto "
                                         value={titulo}
-			                      onChange={e => settitulo(e.target.value)}
+                                        onChange={e => settitulo(e.target.value)}
 
-                                    /> 
+                                    />
                                 </Form.Input>
 
                                 <Form.Input
                                     required
                                     fluid
                                     label='Código do Produto'>
-                                    <InputMask 
+                                    <InputMask
                                         placeholder="Informe o código do produto "
                                         value={codigoDoProduto}
                                         onChange={e => setcodigoDoProduto(e.target.value)}
@@ -78,65 +114,65 @@ export default function FormProduto () {
                                 </Form.Input>
 
                             </Form.Group>
-                            
+
                             <Form.Group>
 
                                 <Form.Input
                                     fluid
                                     label='Descrição'
-                                    >
-                                    <InputMask 
+                                >
+                                    <InputMask
                                         placeholder="Informe a descrição do produto"
                                         value={descricao}
-			                      onChange={e => setdescricao(e.target.value)}
+                                        onChange={e => setdescricao(e.target.value)}
                                     />
                                 </Form.Input>
-                                </Form.Group>
+                            </Form.Group>
 
-                                <Form.Group>
+                            <Form.Group>
                                 <Form.Input
                                     required
                                     fluid
                                     label='Valor Unitario'
                                     value={valorUnitario}
-			                      onChange={e => setvalorUnitario(e.target.value)}
-                                    >
+                                    onChange={e => setvalorUnitario(e.target.value)}
+                                >
                                 </Form.Input>
 
                                 <Form.Input
                                     fluid
                                     label='Tempo de Entrega Mínima em Minutos'
-                                   
-                                    
+
+
                                 >
-                                    <InputMask 
-                                        mask="99" 
+                                    <InputMask
+                                        mask="99"
                                         maskChar={null}
                                         placeholder="30"
                                         value={TempoDeEntregaMinimaEmMinutos}
                                         onChange={e => setTempoDeEntregaMinimaEmMinutos(e.target.value)}
-                                    /> 
+                                    />
                                 </Form.Input>
 
                                 <Form.Input
                                     fluid
                                     label='Tempo de Entrega Máxima em Minutos'
-                                    
+
                                 >
-                                    <InputMask 
-                                        mask="99" 
+                                    <InputMask
+                                        mask="99"
                                         maskChar={null}
                                         placeholder="40"
                                         value={TempoDeEntregaMaximaEmMinutos}
-			                      onChange={e => setTempoDeEntregaMaximaEmMinutos(e.target.value)}
-                                    /> 
+                                        onChange={e => setTempoDeEntregaMaximaEmMinutos(e.target.value)}
+                                    />
                                 </Form.Input>
 
                             </Form.Group>
-                        
+
                         </Form>
-                        
-                        <div style={{marginTop: '4%'}}>
+
+                        <div style={{ marginTop: '4%' }}>
 
                             <Button
                                 type="button"
@@ -149,25 +185,25 @@ export default function FormProduto () {
                                 <Icon name='reply' />
                                 Voltar
                             </Button>
-                                
+
                             <Button
-				inverted
-				circular
-				icon
-				labelPosition='left'
-				color='blue'
-				floated='right'
-				onClick={() => salvar()}
-			>
-				<Icon name='save' />
-				Salvar
-			</Button>
+                                inverted
+                                circular
+                                icon
+                                labelPosition='left'
+                                color='blue'
+                                floated='right'
+                                onClick={() => salvar()}
+                            >
+                                <Icon name='save' />
+                                Salvar
+                            </Button>
 
 
                         </div>
 
                     </div>
-                    
+
                 </Container>
             </div>
         </div>
